d0ace426e610059e1d158848eeb86ec3f9730e91
Andriiko <andrii@digitalidea.studio>
2019-01-10 19:41:30 +0200

    Finish +-*/ operation

--- Code/basic_calc/calc.js
+++ Code/basic_calc/calc.js
@@ -5,21 +5,53 @@ const btnDivide = document.getElementById("divide");
 
 
 
+
+
 btnPlus.addEventListener("click", plus);
 btnMinus.addEventListener("click", minus);
 btnMultiply.addEventListener("click", multiply);
 btnDivide.addEventListener("click", divide);
 
 function plus() {
-    let inp1 = document.getElementById("input1");
+    let inp1 = document.querySelector("#input1");
     let val1 = Number(inp1.value);
-    let inp2 = document.getElementById("input2");
+    let inp2 = document.querySelector("#input2");
     let val2 = Number(inp2.value);
-    let total = document.getElementsByClassName("total");
+    let total = document.querySelector(".total");
     result = val1 + val2;
     total.textContent = result;
 
 
+}
+
+function minus() {
+    let inp1 = document.querySelector("#input1");
+    let val1 = Number(inp1.value);
+    let inp2 = document.querySelector("#input2");
+    let val2 = Number(inp2.value);
+    let total = document.querySelector(".total");
+    result = val1 - val2;
+    total.textContent = result;
+}
+
+function multiply() {
+    let inp1 = document.querySelector("#input1");
+    let val1 = Number(inp1.value);
+    let inp2 = document.querySelector("#input2");
+    let val2 = Number(inp2.value);
+    let total = document.querySelector(".total");
+    result = val1 * val2;
+    total.textContent = result;
+}
+
+function divide() {
+    let inp1 = document.querySelector("#input1");
+    let val1 = Number(inp1.value);
+    let inp2 = document.querySelector("#input2");
+    let val2 = Number(inp2.value);
+    let total = document.querySelector(".total");
+    result = val1 / val2;
+    total.textContent = result;
 }
 
 
